72725c6a7499da74ea24c85a036a1822cf0c1a04
venkatesh-reddi <venkateshreddi@outlook.com>
2024-03-12 11:40:36 +0530

    Added a feature when hit by bullet the alien ship disappears.

--- alien_invasion.py
+++ alien_invasion.py
@@ -99,6 +99,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        # Check fo any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisons = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and keep adding aliens until there's no room left.
