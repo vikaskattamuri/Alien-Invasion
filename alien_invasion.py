8a855727b4bdbff57e680781b3753872bd0c5134
venkatesh-reddi <venkateshreddi@outlook.com>
2024-03-16 09:23:05 +0530

    Modified Speed settings to increase speed with new fleet of alien ships.

--- alien_invasion.py
+++ alien_invasion.py
@@ -179,6 +179,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
