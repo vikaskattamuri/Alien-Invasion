8625357298fb131bde87eef3d5b127b444c7e09c
Vikas Kattamuri <110030220+vikaskattamuri@users.noreply.github.com>
2024-03-16 15:19:31 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 2.5
-        self.bullet_width = 3
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 300
