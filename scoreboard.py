40f9176437e84ae8c95387e623d91887f70452c1
venkatesh-reddi <venkateshreddi@outlook.com>
2024-03-16 09:35:00 +0530

    Created a Scoreboard class to store all score attributes and methods to display score to the screen.

---
+++ scoreboard.py
@@ -0,0 +1,31 @@
+import pygame.font
+
+class Scoreboard:
+    """A class to report scoring information."""
+
+    def __init__(self, ai_game):
+        """Initialize scorekeeping attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stats = ai_game.stats
+
+        # Font settings for scoring information
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Prepare the initial score image.
+        self.prep_score()
+
+    def Prep_score(self):
+        """Turn the score into a rendered image."""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+
+        # Display the score at the top right of the screen.
+        self.score_rect.right = self.screen_rect.right - 30
+        self.score_rect.top = 20
+
+    def show_score(self):
+        """Draw score to the screen."""
+        self.screen.blit(self.score_image, self.score_rect)
