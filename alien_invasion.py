f76b0baa60b1f8ada949cfdb106a89b4f1dd8e26
venkatesh-reddi <venkateshreddi@outlook.com>
2024-03-16 09:57:34 +0530

    Making sure to get score for all hits.

--- alien_invasion.py
+++ alien_invasion.py
@@ -181,7 +181,8 @@ class AlienInvasion:
             self.bullets, self.aliens, True, True)
         
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
 
         if not self.aliens:
