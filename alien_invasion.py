2f204054ec247e1755bac7c934dcbe645400af04
venkatesh-reddi <venkateshreddi@outlook.com>
2024-03-16 06:05:41 +0530

    Disabled Cursor when Playing game and Enabled it once the game ends.

--- alien_invasion.py
+++ alien_invasion.py
@@ -66,6 +66,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """"Check if any aliens have reached the bottom of the screen."""
@@ -103,6 +104,9 @@ class AlienInvasion:
             # Create a new fleet and center the ship.
             self._create_fleet()
             self.ship.center_ship()
+
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
