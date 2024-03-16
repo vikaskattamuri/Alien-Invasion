2e9f1233b8760c97e59a01f9d95c0cabbd13deda
venkatesh-reddi <venkateshreddi@outlook.com>
2024-03-16 09:42:32 +0530

    Fixed the errors in the scoreboard.py.

--- scoreboard.py
+++ scoreboard.py
@@ -17,12 +17,13 @@ class Scoreboard:
         # Prepare the initial score image.
         self.prep_score()
 
-    def Prep_score(self):
+    def prep_score(self):
         """Turn the score into a rendered image."""
         score_str = str(self.stats.score)
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         # Display the score at the top right of the screen.
+        self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 30
         self.score_rect.top = 20
 
